2bb74b5bcfac5dd5898ccfa874d74247d91dc275
Alexander Slavschik <slavshik@me.com>
2023-12-04 22:59:06 +0100

    refactor: add list of files

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,5 @@ import {defineConfig} from "astro/config";
 // https://astro.build/config
 export default defineConfig({
     site: "https://slavshik.github.io",
-    base: "/ruffle-test"
+    base: "/"
 });
