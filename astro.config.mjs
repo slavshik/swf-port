8c2d28ae57be115b8c65d390a5e331eb2e642682
Alexander Slavschik <slavshik@me.com>
2023-12-10 21:55:40 +0100

    feat: added titles for swfs

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,6 @@ import {defineConfig} from "astro/config";
 // https://astro.build/config
 export default defineConfig({
     site: "https://slavshik.github.io",
-    base: "/swf-port/"
+    base: "swf-port/",
+    trailingSlash: "ignore"
 });
